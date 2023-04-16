88f70c9409bc5bd56cb9c32eb60149493c4d27c0
Valery Matskevich <v.matskevich93@gmail.com>
2023-04-16 10:19:29 +0300

    feat: solve extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,39 @@ const { NotImplementedError } = require("../extensions/index.js");
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(str, options) {}
+function repeater(str, options) {
+  if (arguments.length === 1) return String(str);
+  options.repeatTimes =
+    options.repeatTimes === undefined ? 1 : options.repeatTimes;
+  options.separator = options.separator === undefined ? "+" : options.separator;
+  options.addition =
+    options.addition === undefined
+      ? ""
+      : typeof options.addition === "string"
+      ? options.addition
+      : String(options.addition);
+  options.additionRepeatTimes =
+    options.additionRepeatTimes === undefined ? 1 : options.additionRepeatTimes;
+  options.additionSeparator =
+    options.additionSeparator === undefined ? "|" : options.additionSeparator;
+
+  let additionArray = [];
+  let strArray = [];
+
+  for (let i = 0; i < options.additionRepeatTimes; i++) {
+    additionArray.push(options.addition);
+  }
+
+  let combinedAddition = additionArray.join(`${options.additionSeparator}`);
+  str += combinedAddition;
+
+  for (let i = 0; i < options.repeatTimes; i++) {
+    strArray.push(str);
+  }
+  let result = strArray.join(`${options.separator}`);
+
+  return result;
+}
 
 module.exports = {
   repeater,
