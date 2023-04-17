43956e0798d90bf7fb1bc5b992cbb4bb1f45c8ff
Valery Matskevich <v.matskevich93@gmail.com>
2023-04-18 02:59:15 +0300

    feat: fix vigenere

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -49,12 +49,41 @@ class VigenereCipheringMachine {
       }
     }
 
-    return encodeString.join("");
+    return this.direction
+      ? encodeString
+      : encodeString.split("").reverse().join("");
   }
   decrypt(string, key) {
     if (string === undefined || key === undefined)
-      string = string.toUpperCase();
+      throw new Error("Incorrect arguments!");
+    string = string.toUpperCase();
     key = key.toUpperCase();
+
+    let KeyRepetitions = Math.ceil(string.length / key.length);
+    key = key.repeat(KeyRepetitions);
+
+    const codeA = "A".charCodeAt(0);
+    const alphabetCount = 26;
+    let decryptString = [];
+
+    for (let i = 0; i < string.length; i++) {
+      if (string[i] === " ") {
+        decryptString.push(string[i]);
+      } else {
+        let letterIndex = string.charCodeAt(i) - codeA;
+        let offset = key.charCodeAt(i) - codeA;
+
+        decryptString.push(
+          String.fromCharCode(
+            codeA + ((letterIndex - offset + alphabetCount) % alphabetCount)
+          )
+        );
+      }
+    }
+
+    return this.direction
+      ? decryptString
+      : decryptString.split("").reverse().join("");
   }
 }
 
